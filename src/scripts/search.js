aeb13667642bdf05471bcf841af56b5b2cc83d96
Evgeniy Sidliarevich <brodjagka93@gmail.com>
2022-04-19 16:05:09 +0300

    add adaptability of search

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,20 +1,23 @@
+const header = document.querySelector(".header");
 const searchBtn = document.querySelector(".header__search-sm-btn");
 const searchBox = document.querySelector(".header__search-box");
 const basketBtn = document.querySelector(".header__button");
 const logo = document.querySelector(".logo");
 
 
-searchBtn.addEventListener("click", () => {
-    logo.classList.add("disabled");
-    basketBtn.classList.add("disabled");
-    searchBox.classList.add("enabled");
-    searchBtn.classList.add("disabled");
-    searchBox.addEventListener("mouseout", () => {
-        console.log("hi")
+header.addEventListener("click", () => {
+    if (event.target.closest(".header__search-sm-btn") || event.target.closest(".header__search-box.enabled")) {
+        logo.classList.add("disabled");
+        basketBtn.classList.add("disabled");
+        searchBox.classList.add("enabled");
+        searchBtn.classList.add("disabled");
+    } else {
         logo.classList.remove("disabled");
         basketBtn.classList.remove("disabled");
         searchBox.classList.remove("enabled");
         searchBtn.classList.remove("disabled");
-    })
+    }
+
+
 })
 
