4241065dab9d2f5e58670e32b934985fc8fde28b
Andrey <a.zakcharcuk@gmail.com>
2022-04-22 22:08:50 +0300

    button scroll add

---
+++ src/scripts/scroll-arrow.js
@@ -0,0 +1,20 @@
+const offset = 500;
+const buttonArrow = document.querySelector(".button-scroll");
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+window.addEventListener("scroll", () => {
+
+    if (getTop() > offset) {
+        buttonArrow.classList.add("scroll-up--active");
+    } else {
+        buttonArrow.classList.remove("scroll-up--active")
+    }
+})
+
+buttonArrow.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
