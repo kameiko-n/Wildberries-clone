abf47fd071b4ab6259e2c51f7596ce3a05325aea
Elena Davydik <elena.v.davydik@gmail.com>
2022-04-22 21:56:07 +0300

    Изменён быстрый просмотр

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -18,35 +18,29 @@ getProducts().then(products => {
         const productSale = item.sale;
         const productImage = item.imgSrc;
     
-        let templete = 
-                        `<div class="card" id="${productId}">
+        let templete = `<div class="card" id="${productId}">
+        
+                            <div class="card__content" id="cardContent">
+                                <img src="${productImage}" alt="${productName}" class="card__photo">
+                                <a href="#popup" class="card__fast-view" >Быстрый просмотр</a>
+                                <div class="card__bottom">
+                                    <p class="card__sale-value">${productSale}%</p>
+                                    <button class="card__add-in-basket-btn">${basketIcon}</button>
+                                </div>
+                            </div>
 
-                        <div class="card__content">
-                        <img src="${productImage}" alt="${productName}" class="card__photo">
-                        <a href="#popup" class="card__fast-view popup__link">Быстрый просмотр</a>
-                        <div class="card__bottom">
-                            <p class="card__sale-value">${productSale}%</p>
-                            <button class="card__add-in-basket-btn">
-                                ${basketIcon}
-                            </button>
-                        </div>
-                        </div>
+                            <div class="card__prices">
+                                <p class="card__new-price">${productNewPrice}р</p>
+                                <p class="card__old-price">${productOldPrice}р</p>
+                            </div>
+                            <h3 class="card__title">${productName}</h3>
 
-                        <div class="card__prices">
-                        <p class="card__new-price">${productNewPrice}р</p>
-                        <p class="card__old-price">${productOldPrice}р</p>
-                        </div>
-                        <h3 class="card__title">${productName}</h3>
-
-                        <div id="popup" class="popup">
-                        <div class="popup-body">
-                            
-                                <img src="${productImage}" alt="${productName}" class="popup-photo">
-                                <a href="/" class="popup-close close-popup">
-                                ${closeIcon}
-                                </a>
-                           
-                        </div>
+                            <div id="popup" class="popup">
+                                <div class="popup-body">
+                                    <img src="${productImage}" alt="${productName}" class="popup-photo">
+                                    <a href="#cardContent" class="popup-close">${closeIcon}</a>
+                                </div>
+                            </div>
                         </div>`;
         productsItems.insertAdjacentHTML("beforeend", templete);
     })
