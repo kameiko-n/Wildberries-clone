32cb9a94c61063fe1f27b6e1dec6bcaccdabd305
Evgeniy Sidliarevich <brodjagka93@gmail.com>
2022-04-28 14:39:05 +0300

    1234

--- src/scripts/basket.js
+++ src/scripts/basket.js
@@ -1,60 +1,58 @@
-import {body, basketBtn, basketTotal, basketList, basketDelete, basket} from "./variables.js";
+import { body, basketBtn, basketTotal, basketList, basketDelete, basket } from "./variables.js";
 
 let products = [];
 let sum = 0;
 
-const basketObject = {
-  showProductCount: () => {
-    const products = JSON.parse(localStorage.getItem('products'));
-    const productCount = document.querySelector('.product__count');
+const showProductCount = () => {
+    const products = JSON.parse(localStorage.getItem("products"));
+    const productCount = document.querySelector(".product__count");
 
     if (products.length) {
-      productCount.innerText = `${products.length}`;
-      productCount.classList.remove('hide');
+        productCount.innerText = `${products.length}`;
+        productCount.classList.remove("hide");
     } else {
-      productCount.classList.add('hide');
+        productCount.classList.add("hide");
     }
-  },
+};
 
-  addProductToBasket: (product) => {
-    const products = JSON.parse(localStorage.getItem('products'));
-    const find = products.find(item => item.id === product.id);
-    const message = document.querySelector('.message')
+const addProductToBasket = (product) => {
+    const products = JSON.parse(localStorage.getItem("products"));
+    const find = products.find((item) => item.id === product.id);
+    const message = document.querySelector(".message");
 
-    message.classList.add('show');
+    message.classList.add("show");
     setTimeout(() => {
-      message.classList.remove('show')
+        message.classList.remove("show");
     }, 1500);
 
     if (find) {
-      message.innerText = 'Товар уже есть в корзине';
-      return;
+        message.innerText = "Товар уже есть в корзине";
+        return;
     }
 
     products.push(product);
-    localStorage.setItem('products', JSON.stringify(products));
-    basketObject.showProductCount();
+    localStorage.setItem("products", JSON.stringify(products));
+    showProductCount();
+};
 
-  },
+const createProduct = (id, name, newPrice, oldPrice, imgSrc) => {
+    const basketItem = document.createElement("li");
+    const image = document.createElement("img");
+    const productInfo = document.createElement("div");
+    const productName = document.createElement("p");
+    const costs = document.createElement("div");
+    const newCost = document.createElement("p");
+    const oldCost = document.createElement("p");
+    const deleteProduct = document.createElement("button");
 
-  createProduct: (id, name, newPrice, oldPrice, imgSrc) => {
-    const basketItem = document.createElement('li');
-    const image = document.createElement('img');
-    const productInfo = document.createElement('div');
-    const productName = document.createElement('p');
-    const costs = document.createElement('div');
-    const newCost = document.createElement('p');
-    const oldCost = document.createElement('p');
-    const deleteProduct = document.createElement('button');
-
-    basketItem.className = 'basket__item';
-    image.className = 'basket__image';
-    productInfo.className = 'basket__info';
-    productName.className = 'basket__info-name';
-    costs.className = 'basket__info-costs';
-    newCost.className = 'basket__info-new-cost';
-    oldCost.className = 'basket__info-old-cost';
-    deleteProduct.className = 'basket__info-delete';
+    basketItem.className = "basket__item";
+    image.className = "basket__image";
+    productInfo.className = "basket__info";
+    productName.className = "basket__info-name";
+    costs.className = "basket__info-costs";
+    newCost.className = "basket__info-new-cost";
+    oldCost.className = "basket__info-old-cost";
+    deleteProduct.className = "basket__info-delete";
 
     basketItem.dataset.productId = id;
     basketItem.append(image, productInfo);
@@ -67,80 +65,95 @@ const basketObject = {
     oldCost.innerText = `${oldPrice} руб.`;
     deleteProduct.innerText = "Удалить";
 
-    deleteProduct.addEventListener('click', (event) => {
-      const parent = event.target.closest('.basket__item');
-      parent.remove();
-      products = products.filter(product => +product.id !== +parent.dataset.productId);
-      localStorage.setItem('products', JSON.stringify(products));
-      sum -= newPrice;
+    deleteProduct.addEventListener("click", (event) => {
+        const parent = event.target.closest(".basket__item");
+        parent.remove();
+        products = products.filter(
+            (product) => +product.id !== +parent.dataset.productId,
+        );
+        localStorage.setItem("products", JSON.stringify(products));
+        sum -= newPrice;
 
-      if (sum <= 0) {
-        basketTotal.innerText = `В корзине пока ничего нет`;
-      } else {
-        basketTotal.innerText = `Итого ${sum} руб.`;
-      }
+        if (sum <= 0) {
+            basketTotal.innerText = `В корзине пока ничего нет`;
+        } else {
+            basketTotal.innerText = `Итого ${sum} руб.`;
+        }
 
-      basketObject.showProductCount();
-    })
+        showProductCount();
+    });
     return basketItem;
-  },
+};
 
-  initBasket: () => {
-    body.style.overflow = 'hidden';
+const initBasket = () => {
+    body.style.overflow = "hidden";
     sum = 0;
-    const productsFromStorage = JSON.parse(localStorage.getItem('products'));
-    basketObject.showProductCount();
+    const productsFromStorage = JSON.parse(localStorage.getItem("products"));
+    showProductCount();
 
-    if ('products' in localStorage && productsFromStorage && productsFromStorage.length) {
-      basketList.innerHTML = '';
-      productsFromStorage.forEach(product => {
-        basketList.append(basketObject.createProduct(product.id, product.name, product.newPrice, product.oldPrice, product.imgSrc));
-        products.push(product);
-        sum += product.newPrice;
-      })
-      basketTotal.innerText = `Итого ${sum} руб.`;
+    if (
+        "products" in localStorage &&
+        productsFromStorage &&
+        productsFromStorage.length
+    ) {
+        basketList.innerHTML = "";
+        productsFromStorage.forEach((product) => {
+            basketList.append(
+                createProduct(
+                    product.id,
+                    product.name,
+                    product.newPrice,
+                    product.oldPrice,
+                    product.imgSrc,
+                ),
+            );
+            products.push(product);
+            sum += product.newPrice;
+        });
+        basketTotal.innerText = `Итого ${sum} руб.`;
     }
 
-    basket.classList.add('show');
-  },
+    basket.classList.add("show");
+};
 
-  initListeners: () => {
-    basketBtn.addEventListener('click', () => {
-      basketObject.initBasket();
-    })
+ const initListeners = () => {
+    basketBtn.addEventListener("click", () => {
+        initBasket();
+    });
 
-    basketDelete.addEventListener('click', () => {
-      basketList.innerHTML = '';
-      basketTotal.innerText = `В корзине пока ничего нет`;
-      products = [];
-      localStorage.setItem('products', JSON.stringify(products));
-      basketObject.showProductCount();
-    })
-  }
-}
+    basketDelete.addEventListener("click", () => {
+        basketList.innerHTML = "";
+        basketTotal.innerText = `В корзине пока ничего нет`;
+        products = [];
+        localStorage.setItem("products", JSON.stringify(products));
+        showProductCount();
+    });
+};
 
 export const basketOutput = {
-  init: () => {
-    const productsFromStorage = JSON.parse(localStorage.getItem('products'));
+    init: () => {
+        const productsFromStorage = JSON.parse(
+            localStorage.getItem("products"),
+        );
 
-    if (!('products' in localStorage) && !productsFromStorage) {
-      localStorage.setItem('products', JSON.stringify(products));
-    }
+        if (!("products" in localStorage) && !productsFromStorage) {
+            localStorage.setItem("products", JSON.stringify(products));
+        }
 
-    const message = document.createElement('div');
-    const productCount = document.createElement('p');
+        const message = document.createElement("div");
+        const productCount = document.createElement("p");
 
-    message.className = 'message';
-    productCount.className = 'product__count';
-    message.innerText = 'Товар успешно добавлен в корзину';
+        message.className = "message";
+        productCount.className = "product__count";
+        message.innerText = "Товар успешно добавлен в корзину";
 
-    body.append(message);
-    basketBtn.append(productCount);
+        body.append(message);
+        basketBtn.append(productCount);
 
-    basketObject.initListeners();
-    basketObject.showProductCount();
-  },
-  addProduct: (product) => {
-    basketObject.addProductToBasket(product);
-  }
-}
+        initListeners();
+        showProductCount();
+    },
+    addProduct: (product) => {
+        addProductToBasket(product);
+    },
+};
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,55 +1,12 @@
-import {productsItems} from "./variables.js";
-import { basketIconPlus, basketIconCheck, closeIcon } from "./variablesCardIcons.js";
+import {productsBox} from "./variables.js";
+import { basketIconPlus, basketIconCheck, closeIcon } from "./variables.js";
 import { basketOutput } from "./basket.js";
 import { buttonArrow } from "./scroll-arrow.js";
+import { addProductsCard, getProducts } from "./add_products_card.js";
 
-
-export const getProducts = () => {
-  return new Promise((resolve, reject) => {
-    fetch("https://625edd553b039517f1fdb9e1.mockapi.io/products")
-      .then((response) => response.json())
-      .then((users) => resolve(users));
-  });
-};
 
 getProducts()
-  .then((products) => {
-    products.forEach((item) => {
-      const productId = item.id;
-      const productName = item.name;
-      const productOldPrice = item.oldPrice;
-      const productNewPrice = item.newPrice;
-      const productSale = item.sale;
-      const productImage = item.imgSrc;
-
-      let templete = `<div class="card" data-id="${productId}">
-        
-                            <div class="card__content" id="cardContent">
-                                <img src="${productImage}" alt="${productName}" class="card__photo">
-                                <a href="#popup" class="card__fast-view" >Быстрый просмотр</a>
-                                <div class="card__bottom">
-                                    <p class="card__sale-value">${productSale}%</p>
-                                    <button class="card__add-in-basket-btn">${basketIconPlus}</button>
-                                </div>
-                            </div>
-
-                            <div class="card__prices">
-                                <p class="card__new-price">${productNewPrice}р</p>
-                                <p class="card__old-price">${productOldPrice}р</p>
-                            </div>
-                            <h3 class="card__title">${productName}</h3>
-
-                            <div id="popup" class="popup">
-                                <div class="popup-body">
-                                    <img src="${productImage}" alt="${productName}" class="popup-photo">
-                                    <a href="#" class="popup-close">${closeIcon}</a>
-                                </div>
-                            </div>
-                        </div>`;
-      productsItems.insertAdjacentHTML("beforeend", templete);
-    });
-    return products;
-  })
+  .then((products) => addProductsCard(products))
   .then((products) => {
     document.querySelectorAll(".card__add-in-basket-btn").forEach((btn) => {
       btn.addEventListener("click", (event) => {
